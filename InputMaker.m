cd317917131a08dc74cf8cf731a249c2438b2ffd
fyousef1 <fyousef1@TCLASGS-807QHQ2.asurite.ad.asu.edu>
2018-09-11 14:16:23 -0700

    made inputmaker function

---
+++ InputMaker.m
@@ -0,0 +1,17 @@
+function InputMaker
+% InputMaker makes the input data and parameters needed for Windsurf model
+% and saves them as a mat file.
+%
+% Fateme Yousefi Lalimi, Arizona State University, Sep 2018
+%
+%-------------------------------------------------------------------------- 
+
+%-------------- wind parameters
+data.wind.threshold = 4;
+data.wind.dir = [90;90;90;90;90;90];
+data.wind.speed = [5;5;5;5;5;5];
+data.wind.
+data.wind.
+data.wind.
+data.wind.
+data.wind.
